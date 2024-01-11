bbc3575d16583b11b22d37b23b355163b247db4b
Bagus Rinu Pangayom <bagusrinup@gmail.com>
2024-01-11 16:11:47 +0700

    refactor: add footerComp in App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import AnalyticsComp from './components/analyticsComp';
 import CardsComp from './components/cardsComp';
+import FooterComp from './components/footerComp';
 import HeroComp from './components/heroComp';
 import NavbarComp from './components/navbarComp';
 import NewsLetterComp from './components/newsLetterComp';
@@ -13,6 +14,7 @@ function App() {
             <AnalyticsComp />
             <NewsLetterComp />
             <CardsComp />
+            <FooterComp />
         </>
     );
 }
