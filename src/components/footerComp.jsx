c98a3daf882bc7eb9eb8af9e3169d7453711a1e0
Bagus Rinu Pangayom <bagusrinup@gmail.com>
2024-01-11 16:10:47 +0700

    add: footer component

---
+++ src/components/footerComp.jsx
@@ -0,0 +1,13 @@
+const FooterComp = () => {
+    return (
+        <>
+            <div>
+                <div>
+                    <p>footer</p>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default FooterComp;
